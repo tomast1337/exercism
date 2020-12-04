56f58e751c252e21e9cc4845dd364771550bf6ac
Tomast1337 <vycasnicolas@gmail.com>
2020-12-04 19:36:21 -0300

    Rust Prime Factors 🔢➗🧐

---
+++ rust/prime-factors/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn factors(n: u64) -> Vec<u64> {
+    let mut result:Vec<u64> = Vec::new();
+    let mut curent_num:u64 = n;
+    while curent_num != 1{
+        let mut d = 1;
+        //=curent_num  inclusive end wont work, dont know why
+        for i in 2 .. (curent_num+1) {
+            if is_prime(i) && curent_num % i == 0 {
+                result.push(i);
+                d = i;
+                break;
+            }
+        }
+        curent_num = curent_num / d;
+    }
+    return result;
+}
+fn is_prime(n: u64) -> bool {
+    let mut divisors_count:u64 = 0;
+    for i in (2..n).rev() {
+        if n % i == 0 {
+            divisors_count += 1;
+        }
+    }
+    return divisors_count == 0;
+}
