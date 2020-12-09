7ae68a9f9a63ad14acf2270b8585a6f3f2ef3989
Tomast1337 <vycasnicolas@gmail.com>
2020-12-09 14:20:19 -0300

    Rust Fixed BOB 💁‍♀️

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,18 +1,10 @@
 pub fn reply(message: &str) -> &str {
-    let var = &message.replace(&[' ','\t','\r',':','}','{','(',')'][..], "");
-    println!("{}",var);
-    if var.is_empty() {
-        return "Fine. Be that way!"
-    }else{
-        if var.to_uppercase() == var.to_string() && var.len() > 1{
-            if var.chars().last() == Some('?') {
-                return "Calm down, I know what I'm doing!"
-            }
-            return "Whoa, chill out!"
-        }
-        if var.chars().last() == Some('?') {
-            return "Sure."
-        }
-    }
+    let var = &message.replace(&[' ','\t','\r'][..], "");
+    if var.is_empty() { return "Fine. Be that way!" }
+    let is_question = var.chars().last() == Some('?');
+    let is_all_caps = (var.to_uppercase() == var.to_string())&& var.chars().any(|c| matches!(c, 'A'..='Z'));
+    if is_all_caps && is_question { return "Calm down, I know what I'm doing!" }
+    if is_question { return "Sure." }
+    if is_all_caps { return "Whoa, chill out!" }
     return "Whatever.";
 }
