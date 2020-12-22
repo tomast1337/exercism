99614f0fae70a52c1019c7a3140499f61a98f372
Tomast1337 <vycasnicolas@gmail.com>
2020-12-22 19:51:48 -0300

    Java☕ Raindrops🌧

---
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -0,0 +1,12 @@
+class RaindropConverter {
+
+    String convert(int number) {
+        String result = "";
+        if (number % 3 == 0) result +="Pling";
+        if (number % 5 == 0) result +="Plang";
+        if (number % 7 == 0) result +="Plong";
+        if (result == "") return String.valueOf(number);
+        return result;
+    }
+
+}
