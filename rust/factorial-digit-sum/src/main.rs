8e3e3c7a4e5a0e14ae7a72fae652d6086327100b
Tomast1337 <vycasnicolas@gmail.com>
2021-01-26 12:46:28 -0300

    Rust :crab: Factorial Digit Sum

--- rust/factorial-digit-sum/src/main.rs
+++ rust/factorial-digit-sum/src/main.rs
@@ -5,6 +5,13 @@ fn main() {
     }
     println!("\n\nTempo de execução: {:?}\n\n", time.elapsed().unwrap());
 }
-fn factorial_digit_sum(n:u64) -> u64{
-    (1..n).sum()
+fn factorial_digit_sum(n:u32) -> u64{
+    let mut factorial: num_bigint::BigInt = num_traits::FromPrimitive::from_u32(1).unwrap();
+    for i in 1..n {
+        factorial = factorial * i;
+    }
+    factorial.to_string()
+            .chars()
+            .map(|d| d.to_digit(10).unwrap_or(0) as u64)
+            .sum::<u64>()
 }
