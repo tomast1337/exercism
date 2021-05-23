d3ba4f621f1d074ffb499634560efd4f0e6bae70
Tomast1337 <vycasnicolas@gmail.com>
2021-05-23 01:24:03 -0300

    Alterado o nome de Aresta para Vértice

--- cpp/grafo/main.cpp
+++ cpp/grafo/main.cpp
@@ -1,151 +1,193 @@
 #include <iostream>
-template <typename Struct> struct Item{
+
+template<typename Struct>
+struct Item {
     Struct dado;
-    Item* proximo;
-    Item(){}
-    Item(Struct dado){
+    Item *proximo;
+
+    Item() {}
+
+    Item(Struct dado) {
         this->dado = dado;
     }
-    Item(Struct dado,Item* proximo){
+
+    Item(Struct dado, Item *proximo) {
         this->proximo = proximo;
         this->dado = dado;
     }
 };
-template <typename Struct> struct  Lista{
-    Item<Struct>* cabeca;
-    void remover(Item<Struct>* item){
-        if(item == cabeca){
-            Item<Struct>* antigo = cabeca;
-            cabeca = cabeca->proximo == nullptr? cabeca = nullptr : cabeca->proximo;;
+
+template<typename Struct>
+struct Lista {
+    Item<Struct> *cabeca;
+
+    void remover(Item<Struct> *item) {
+        if (item == cabeca) {
+            Item<Struct> *antigo = cabeca;
+            cabeca = cabeca->proximo == nullptr ? cabeca = nullptr : cabeca->proximo;;
             delete antigo;
-        }else{
-            Item<Struct>* anterior = cabeca;
-            while (anterior->proximo != nullptr && anterior->proximo != item){
+        } else {
+            Item<Struct> *anterior = cabeca;
+            while (anterior->proximo != nullptr && anterior->proximo != item) {
                 anterior = anterior->proximo;
             }
-            if(anterior->proximo != nullptr){
+            if (anterior->proximo != nullptr) {
                 anterior->proximo = anterior->proximo->proximo;
                 delete item;
             }
         }
     }
-    void inserir(Struct* item){
-        if(cabeca == nullptr){
+
+    void inserir(Item<Struct> *item) {
+        if (cabeca == nullptr) {
             cabeca = item;
-        }else{
-            Item<Struct> ulitmo = cabeca;
-            while (ulitmo->proximo != nullptr){
+        } else {
+            Item<Struct> *ulitmo = cabeca;
+            while (ulitmo->proximo != nullptr) {
                 ulitmo = ulitmo->proximo;
             }
-            ulitmo.proximo = item;
+            ulitmo->proximo = item;
         }
     }
-    Item<Struct>* ultimo(){
-        Item<Struct>* ultimo = cabeca;
+
+    Item<Struct> *ultimo() {
+        Item<Struct> *ultimo = cabeca;
         while (ultimo->proximo != nullptr)
             ultimo = ultimo->proximo;
         return ultimo;
     }
 };
-struct Grafo{
-    int numeroArestas;
+
+struct Grafo {
+    int numeroVertices;
     Lista<Lista<char>> listaListasAdjacencia;
 
-    Grafo(){
-        this->numeroArestas = 0;
+    Grafo() {
+        this->numeroVertices = 0;
         this->listaListasAdjacencia = *new Lista<Lista<char>>;
     }
-    Lista<char>* getListaAdj(char aresta){
-        if(existe(aresta)){
-            Item<Lista<char>>* ultimo = listaListasAdjacencia.cabeca;
-            while (ultimo->proximo != nullptr){
-                if(ultimo->dado.cabeca->dado == aresta)
+
+    Lista<char> *getVizinhos(char vertice) {
+        if (existe(vertice)) {
+            Item<Lista<char>> *ultimo = listaListasAdjacencia.cabeca;
+            while (ultimo->proximo != nullptr) {
+                if (ultimo->dado.cabeca->dado == vertice)
                     return &ultimo->dado;
                 ultimo = ultimo->proximo;
             }
         } else
             return nullptr;
     }
-    void imprimir(){
-        if(listaListasAdjacencia.cabeca != nullptr){
-            Item<Lista<char>>* aresta = listaListasAdjacencia.cabeca;
-            while (aresta != nullptr){
-                std::cout<<aresta->dado.cabeca->dado<<": ";
-                Item<char>* adjacencia = aresta->dado.cabeca;
-                if (adjacencia->proximo == nullptr)
-                    std::cout<<"sem vizinhos";
-                else while (adjacencia->proximo != nullptr){
-                    std::cout<<adjacencia->dado;
-                    adjacencia = adjacencia->proximo;
 
-                    if (adjacencia->proximo == nullptr) std::cout<<"";
-                    else std::cout<<",";
+    void imprimirAdjacencia(char vertice) {
+        if (existe(vertice)) {
+            Item<char> *vizinho = getVizinhos(vertice)->cabeca;
+            if (vizinho->proximo == nullptr) {
+                std::cout << "sem vizinhos";
+            } else {
+                std::cout << vizinho->dado << ": ";
+                vizinho = vizinho->proximo;
+                while (vizinho != nullptr) {
+                    std::cout << vizinho->dado;
+                    vizinho = vizinho->proximo;
+                    if (vizinho == nullptr) std::cout << "";
+                    else std::cout << ",";
                 }
-                std::cout<<std::endl;
-                aresta = aresta->proximo;
             }
-        }else{
-            std::cout<<"O grafo esta vazio"<<std::endl;
-        }
+        } else
+            std::cout << "Vértice " << vertice << " não existe";
     }
-    bool existe(char aresta){
-        if(listaListasAdjacencia.cabeca == nullptr) return false;
-        Item<Lista<char>>* ultimo = listaListasAdjacencia.cabeca;
-        while (ultimo->proximo != nullptr){
-            if(ultimo->dado.cabeca->dado == aresta) return true;
-            ultimo = ultimo->proximo;
-        }
-        return false;
-    }
-    void adicionarAresta(char aresta){
-        Item<Lista<char>> * novo = new Item<Lista<char>>;
-        novo->dado = *new Lista<char>;
-        novo->dado.cabeca = new Item<char>;
-        novo->dado.cabeca->dado = aresta;
-        if(listaListasAdjacencia.cabeca == nullptr) listaListasAdjacencia.cabeca = novo;
-        else{
-            Item<Lista<char>>* ultimo = listaListasAdjacencia.cabeca;
-            while (ultimo->proximo != nullptr) ultimo = ultimo->proximo;
-            ultimo->proximo = novo;
-        }
-        numeroArestas ++;
-        std::cout<<"Adicionado aresta:"<<aresta<<std::endl;
-    }
-    void removerAresta(char aresta){
-        numeroArestas --;
-    }
-    void AdicionaVizinho(char arestaA, char arestaB){
-        bool existeA = existe(arestaA), existeB = existe(arestaB);
-        if (existeA && existeB){
-            if(arestaA == arestaB){
-                Item<char> *ptrLista = getListaAdj(arestaA)->ultimo();
-                ptrLista->proximo = new Item<char>(arestaB);
-            }else {
-                Item<char> *ptrListaA = getListaAdj(arestaA)->ultimo();
-                Item<char> *ptrListaB = getListaAdj(arestaB)->ultimo();
-                ptrListaA->proximo = new Item<char>(arestaB);
-                ptrListaB->proximo = new Item<char>(arestaA);
+
+    void imprimirGrafo() {
+        std::cout << "Imprimindo grafo" << std::endl;
+        if (listaListasAdjacencia.cabeca != nullptr) {
+            Item<Lista<char>>* vertice = listaListasAdjacencia.cabeca;
+            while (vertice != nullptr) {
+                imprimirAdjacencia(vertice->dado.cabeca->dado);
+                std::cout << std::endl;
+                vertice = vertice->proximo;
             }
-        }else{
-            std::cout<<"Não é possível, uma das arestas ou as duas não existem "<<std::endl;
+            std::cout << "Total vertices: " << numeroVertices << std::endl;
+        } else {
+            std::cout << "O grafo esta vazio" << std::endl;
         }
     }
-    Lista<char>* vizinhos(char aresta){
-        return new Lista<char>();
-    }
-    bool TesteVizinhaca(char arestaA,char arestaB){
+
+    bool existe(char vertice) {
+        Item<Lista<char>> *proximo = listaListasAdjacencia.cabeca;
+        while (proximo != nullptr) {
+            if (proximo->dado.cabeca->dado == vertice) return true;
+            proximo = proximo->proximo;
+        }
         return false;
     }
 
+    void adicionarVertice(char vertice) {
+        if (!existe(vertice)) {
+            Item<Lista<char>> *novo = new Item<Lista<char>>;
+            novo->dado = *new Lista<char>;
+            novo->dado.inserir(new Item<char>(vertice));
+            if (listaListasAdjacencia.cabeca == nullptr) listaListasAdjacencia.cabeca = novo;
+            else {
+                Item<Lista<char>> *ultimo = listaListasAdjacencia.ultimo();
+                ultimo->proximo = novo;
+            }
+            numeroVertices++;
+            std::cout << "Adicionado vertice:" << vertice << std::endl;
+        } else {
+            std::cout << "Vértice:" << vertice << " já existe" << std::endl;
+        }
+
+    }
+
+    void removerVertice(char vertice) {
+        numeroVertices--;
+    }
+
+    void AdicionaVizinhaca(char verticeA, char verticeB) {
+        bool existeA = existe(verticeA), existeB = existe(verticeB);
+        if (existeA && existeB) {
+            if (verticeA == verticeB) {
+                getVizinhos(verticeA)->inserir(new Item<char>(verticeB));
+                std::cout << verticeA << " é vizinho de " << verticeB << std::endl;
+            } else {
+                getVizinhos(verticeA)->inserir(new Item<char>(verticeB));
+                getVizinhos(verticeB)->inserir(new Item<char>(verticeA));
+                std::cout << verticeA << " e " << verticeB << " são vizinhos" << std::endl;
+            }
+        } else {
+            std::cout << "Não é possível, um dos vertices ou as duas não existem " << std::endl;
+        }
+    }
+
+    bool TesteVizinhaca(char verticeA, char verticeB) {
+    }
 };
 
 int main() {
-    Grafo* grafo = new Grafo;
-    grafo->adicionarAresta('A');
-    grafo->adicionarAresta('B');
-    grafo->adicionarAresta('C');
-    //grafo->AdicionaVizinho('A', 'B');
-    //grafo->AdicionaVizinho('A', 'A');
-    grafo->AdicionaVizinho('C', 'C');
-    grafo->imprimir();
+    Grafo *grafo = new Grafo;
+    grafo->adicionarVertice('A');
+    grafo->adicionarVertice('B');
+    grafo->adicionarVertice('C');
+    grafo->adicionarVertice('D');
+    grafo->adicionarVertice('E');
+    std::cout << std::endl;
+    grafo->AdicionaVizinhaca('B', 'A');
+    grafo->AdicionaVizinhaca('C', 'D');
+    grafo->AdicionaVizinhaca('A', 'A');
+    grafo->AdicionaVizinhaca('C', 'C');
+    std::cout << std::endl;
+    grafo->imprimirGrafo();
+    std::cout << std::endl;
+    grafo->adicionarVertice('G');
+    grafo->adicionarVertice('H');
+    grafo->adicionarVertice('K');
+    std::cout << std::endl;
+    grafo->AdicionaVizinhaca('G', 'A');
+    grafo->AdicionaVizinhaca('G', 'B');
+    grafo->AdicionaVizinhaca('G', 'D');
+    grafo->AdicionaVizinhaca('G', 'E');
+    std::cout << std::endl;
+    grafo->imprimirGrafo();
+    std::cout << std::endl;
 }
