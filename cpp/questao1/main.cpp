e5d9b3e4c6aeae46b36f56a7cbec012d05e5bbe8
Tomast1337 <vycasnicolas@gmail.com>
2021-05-26 17:41:29 -0300

    Pequenas alterações

--- cpp/questao1/main.cpp
+++ cpp/questao1/main.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
+
 struct Cadeia {
     char *cadeia;
     int tamanho;
+
     Cadeia(char *cadeia, int tamanho) {
         this->cadeia = cadeia;
         this->tamanho = tamanho;
     }
 };
-void permutar(char *mascara, int tamanho, int n, Cadeia* cadeia) {
+
+void permutar(Cadeia *mascara, int tamanho, int n, Cadeia *cadeia) {
     if (tamanho == 1) {
-        int iCadeia = 0;
-        for (int i = 0; i < n; i++){
-            if(mascara[i] == '#'){
+        for (int i = 0, iCadeia = 0; i < n; i++) {
+            if (mascara->cadeia[i] == '#') {
                 std::cout << cadeia->cadeia[iCadeia];
                 iCadeia++;
-            }else
+            } else
                 std::cout << "-";
         }
-        printf("\n");
+        std::cout << std::endl;
         return;
     }
     for (int i = 0; i < tamanho; i++) {
         permutar(mascara, tamanho - 1, n, cadeia);
-        if (tamanho % 2 == 1){
-            char aux = mascara[0];
-            mascara[0] = mascara[tamanho - 1];
-            mascara[tamanho - 1] = aux;
-        }
-        else {
-            char aux = mascara[i];
-            mascara[i] = mascara[tamanho - 1];
-            mascara[tamanho - 1] = aux;
+        if (tamanho % 2 == 1) {
+            char aux = mascara->cadeia[i];
+            mascara->cadeia[i] = mascara->cadeia[tamanho - 1];
+            mascara->cadeia[tamanho - 1] = aux;
+        } else {
+            char aux = mascara->cadeia[i];
+            mascara->cadeia[i] = mascara->cadeia[tamanho - 1];
+            mascara->cadeia[tamanho - 1] = aux;
         }
     }
 }
+
 void percorrer(Cadeia *cadeiaA, Cadeia *cadeiaB) {
     if (cadeiaA->tamanho < cadeiaB->tamanho) {
         Cadeia *aux = cadeiaA;
@@ -42,11 +44,20 @@ void percorrer(Cadeia *cadeiaA, Cadeia *cadeiaB) {
     }
     std::cout << "Maior Cadeia: " << cadeiaA->cadeia << std::endl;
     std::cout << "Menor Cadeia: " << cadeiaB->cadeia << std::endl;
-    char *mascara = new char[cadeiaA->tamanho];
-    for (int i = 0; i < cadeiaA->tamanho; i++) mascara[i] = i >= cadeiaB->tamanho ? '-' : '#';
-    std::cout<<"Permutação de maneiras de inserir espaço em branco:"<<std::endl;
-    permutar(mascara, cadeiaA->tamanho, cadeiaA->tamanho, cadeiaB);
+
+    Cadeia *mascara = new Cadeia(new char[cadeiaA->tamanho], cadeiaA->tamanho);
+    for (int i = 0; i < cadeiaA->tamanho; i++)
+        mascara->cadeia[i] = i >= cadeiaB->tamanho ? '-' : '#';
+
+    int n = cadeiaA->tamanho;
+    int k = n - cadeiaB->tamanho;
+    Cadeia *lista[k * (n - k + 1) * (n - k)];
+
+    std::cout << "Permutação de maneiras de inserir espaço em branco:" << std::endl;
+
+    //permutar(mascara, cadeiaA->tamanho, cadeiaA->tamanho, cadeiaB);
 }
+
 int main() {
     Cadeia *cadeiaA = new Cadeia("GCTA", 4);
     Cadeia *cadeiaB = new Cadeia("AG", 2);
