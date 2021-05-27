a06cd13ec98ec440104e609d58175eb93bb107ac
Tomast1337 <vycasnicolas@gmail.com>
2021-05-27 05:01:49 -0300

    Insetion

--- cpp/magic.cpp
+++ cpp/magic.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-void passarPorValor(int valor){}
-void passarPorMagica(){
-    int magica;
-    std::cout<<"Magica:"<<magica<<std::endl;
+void insetionSort(char *array,int tamanho){
+    for (int i = 1, j; i < tamanho; i++) {
+        char chave = array[i];
+        j = i - 1;
+        while (j >= 0 && array[j] > chave) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = chave;
+    }
 }
+
 int main(){
-    int valor = 100;
-    passarPorValor(valor);
-    passarPorMagica();
+    char valores[] = "NicolasAmaLetica";
+    std::cout<<valores<<std::endl;
+    insetionSort(valores,16);
+    std::cout<<valores<<std::endl;;
+
     return 0;
 }
