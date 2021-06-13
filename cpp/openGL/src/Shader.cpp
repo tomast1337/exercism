66b0a89e2eb3a4ca38cc25d48dc8a8e2f40b9a4e
Tomast1337 <vycasnicolas@gmail.com>
2021-06-13 03:41:34 -0300

    Implementado classe Shader

---
+++ cpp/openGL/src/Shader.cpp
@@ -0,0 +1,96 @@
+//
+// Created by tomast1337 on 13/06/2021.
+//
+
+#include "Shader.h"
+
+unsigned int compileShader(int type, const char *source);
+
+unsigned int criarShaderProgram(unsigned int vertex, unsigned int fragment);
+
+Shader::Shader(const char *vertexPath, const char *fragmentPath) {
+    std::ifstream vertexFile, fragmentFile;
+    std::string vertexCode, fragmentCode;
+
+    vertexFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fragmentFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+
+        vertexFile.open(vertexPath);
+        fragmentFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        vShaderStream << vertexFile.rdbuf();
+        fShaderStream << fragmentFile.rdbuf();
+        vertexFile.close();
+        fragmentFile.close();
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+
+    } catch (std::ifstream::failure e) {
+        std::cout << "File Stream erro: " << e.what() << std::endl;
+    }
+    const char *vertexShaderCode = vertexCode.c_str();
+    const char *fragmentShaderCode = fragmentCode.c_str();
+
+    unsigned int vertex = compileShader(GL_VERTEX_SHADER, vertexShaderCode);
+    unsigned int fragment = compileShader(GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+    this->ID = criarShaderProgram(vertex, fragment);
+}
+
+void Shader::use() {
+    glUseProgram(this->ID);
+}
+
+void Shader::setBool(const std::string &name, bool value) const {
+    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), (int) value);
+}
+
+void Shader::setInt(const std::string &name, int value) const {
+    glUniform1i(glGetUniformLocation(this->ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const {
+    glUniform1f(glGetUniformLocation(this->ID, name.c_str()), value);
+}
+
+unsigned int criarShaderProgram(unsigned int vertex, unsigned int fragment) {
+    int status;
+    char log[512];
+    unsigned int shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertex);
+    glAttachShader(shaderProgram, fragment);
+
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+    if (!status) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, log);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << log << std::endl;
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    return shaderProgram;
+}
+
+unsigned int compileShader(int type, const char *source) {
+    unsigned int shader = glCreateShader(type);
+
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+    // Checar por erros e se existir erros imprimir log
+    int statusCompilacao;
+    char Log[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &statusCompilacao);
+    if (!statusCompilacao) {
+        glGetShaderInfoLog(shader, 512, nullptr, Log);
+        if (type == GL_VERTEX_SHADER)
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << Log << std::endl;
+        if (type == GL_FRAGMENT_SHADER)
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << Log << std::endl;
+    }
+    return shader;
+}
---
+++ cpp/openGL/src/Shader.h
@@ -0,0 +1,23 @@
+#ifndef OPENGL_SHADER_H
+#define OPENGL_SHADER_H
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader {
+public:
+    unsigned int ID;
+    Shader(const char* vertexPath, const char* fragmentPath);
+    void use();
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
+
+#endif //OPENGL_SHADER_H
